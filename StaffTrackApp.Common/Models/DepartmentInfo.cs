c9a22f51762bf60aba4357759276ca16e59aa97e
Антон <@DESKTOP-SOJTG7Q>
2023-12-19 15:29:10 +0200

    Added models

---
+++ StaffTrackApp.Common/Models/DepartmentInfo.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace StaffTrackApp.Common.Models
+{
+    public class DepartmentInfo
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public decimal AverageSalary { get; set; }
+    }
+
+}
---
+++ StaffTrackApp.Common/Models/DepartmentsSalaryRanges.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace StaffTrackApp.Common.Models
+{
+    public class DepartmentsSalaryRanges
+    {
+        public int DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+        public decimal MinimumSalary { get; set; }
+        public decimal MaximumSalary { get; set; }
+    }
+}
