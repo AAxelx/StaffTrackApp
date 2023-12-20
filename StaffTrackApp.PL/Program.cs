26f3e78e960a067a7792d5a4bb2d7606a0399479
Антон <@DESKTOP-SOJTG7Q>
2023-12-20 15:27:18 +0200

    Added configurations and registered services

--- StaffTrackApp.PL/Program.cs
+++ StaffTrackApp.PL/Program.cs
@@ -1,15 +1,36 @@
+using StaffTrackApp.DAL.DataAccess.Abstractions;
+using StaffTrackApp.DAL.DataAccess;
+using StaffTrackApp.Common.Configurations;
+using StaffTrackApp.SAL.Services;
+using StaffTrackApp.SAL.Services.Abstractions;
+using StaffTrackApp.SAL.Helpers;
+using StaffTrackApp.SAL.Helpers.Abstractions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var configurationBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+var configuration = configurationBuilder.Build();
+
 builder.Services.AddRazorPages();
+builder.Services.Configure<CompanyConfiguration>(configuration.GetSection("CompanyConfig"));
+builder.Services.Configure<ExportConfiguration>(configuration.GetSection("ExportSettings"));
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddTransient<ITextFileWriter, TextFileWriter>();
+
+builder.Services.AddTransient<IEmployeeService, EmployeeService>();
+
+builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
